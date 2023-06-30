9c90b50640a544c1a93e68adbe60a2826755df6d
Edukons <eduardo.kons03@gmail.com>
2023-06-30 11:48:47 -0300

    inicio do layout

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <aside>
+    <nav class="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </aside>
+  <main>
+    <router-view/>
+  </main>
+  <footer>FOOTER</footer>
+</template>
+
+<style scoped>
+body{
+  background: url("https://img.olhardigital.com.br/wp-content/uploads/2023/03/Star-Wars.jpg");
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+todos{
+  display: flex;
+  position: relative;
+}
+</style>
